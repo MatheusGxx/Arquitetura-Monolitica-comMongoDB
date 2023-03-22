264062afde09495ffb47e2217e8f8aa03c389168
MatheusGxx <matheussoaresfff02@gmail.com>
2023-03-22 16:41:32 -0300

    Finalizaçao do projeto
    finalizaçao do sistema do sistema de cadastro e ediçao e exclusao dos clientes.

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -32,15 +32,71 @@ function index(req, res){
    })
 }
 
-   async function listUsers(req, res){
+async function listUsers(req, res){
       const Users = await  CustomersModel.find() // pegando um metodo do mongoose que esta dentro do customersModel aonde nos da o poder de selecionar determinado usuario ou todos usuarios, () = Todos os usuarios
       res.render('listUsers',{
          title: 'Listagem de usuarios',
-         users: Users
+         users:Users
       })
-   }
-module.exports = {
+}
+
+async function indexEdit(req, res){  // lendo e pegando o id
+   const { id } = req.query // lendo o id do usuario no banco de dados e na url
+
+   const user = await CustomersModel.findById(id) // pegando o id do usuario no banco de dados
+
+   res.render('edit',{
+      title: 'Ediçao de clientes',
+      user, //renderizando o id do usuario na tela
+   })
+}
+
+
+ async function edit(req,res){
+   const { // Chamando o servidor e forçando ele a ler os itens enviados do formulario
+      name,
+      age,
+      email,
+     } = req.body
+
+     const { id } = req.params // Lendo o ID do formulario ( Params é para ler o ID do formulario e query é para ler o ID da URL)
+
+     const user = await CustomersModel.findById(id) // pegando o IP do forulario
+     
+
+     // Alterando as mudanças ja feitas pelo usuario no formulario
+     user.name = name
+     user.age = age
+     user.email = email
+     user.save()
+
+     res.render('edit',{ // Após alterar as mudanças do usuario no formulario, rendiza a pagina de edit com uma mensagem encima do formulario.
+      title: 'Ediçao feita com sucesso',
+      user, //renderizando o id do usuario na tela
+      message: 'Usuario Alterado com sucesso'
+   })
+}
+async function remover(req, res){
+ const { id }  = req.params
+ 
+  const remover = await CustomersModel.deleteOne({ _id: id})
+   
+  console.log(remover)
+
+  if(remover) {
+   res.redirect('/list')
+ }
+  else{
+   res.send('Erro ao apagar o cliente')
+ }
+}
+
+ 
+module.exports = {   
  index,
  add,
  listUsers,
+ indexEdit,
+ edit,
+ remover,
 }
--- src/routes/rotas.js
+++ src/routes/rotas.js
@@ -6,6 +6,9 @@ const IndexController = require('../controllers/index')
 
 // rotas
 router.get('/', IndexController.index)
+
+
+//Registro
 router.get('/register', CustomersConstroller.index)
 router.post('/register/add', CustomersConstroller.add)    //CONTROLLER  - Responsavel por receber os dados salvar eles receber no banco de dados e devolver uma resposta
 
@@ -13,8 +16,17 @@ router.post('/register/add', CustomersConstroller.add)    //CONTROLLER  - Respon
 // Listar usuarios
 
 router.get('/list', CustomersConstroller.listUsers)
+    
+// Editar Usuarios
 
+router.get('/edit', CustomersConstroller.indexEdit)
+router.post('/edit/:id', CustomersConstroller.edit)
 
+// Removendo Usuarios
+
+router.get('/remove/:id', CustomersConstroller.remover)
 
 
 module.exports = router
+
+
